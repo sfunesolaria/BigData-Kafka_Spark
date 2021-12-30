076b8d73a6930881e0c81001a289f0e1323a3c15
sfunesolaria <76247201+sfunesolaria@users.noreply.github.com>
2021-12-30 04:01:20 +0100

    add EX2_1_2

---
+++ PEC5_sfunesolaria_2_1_2.py
@@ -0,0 +1,64 @@
+import findspark
+findspark.init()
+
+from pyspark import SparkConf, SparkContext, SQLContext, HiveContext
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import explode
+from pyspark.sql.functions import split
+
+conf = SparkConf()
+conf.setMaster("local[1]")
+sc = SparkContext(conf=conf)
+print(sc.version)
+
+# Introducid el nombre de la app PEC5_ seguido de vuestro nombre de usuario
+spark = SparkSession \
+    .builder \
+    .appName("PEC5_sfunesolaria") \
+    .getOrCreate()
+
+# Creamos el DataFrame representando el streaming de las lineas que nos entran por host:port
+linesDF = spark\
+    .readStream\
+    .format('socket')\
+    .option('host', 'localhost')\
+    .option('port', 20068)\
+    .load()
+
+# Separamos las lineas en palabras en un nuevo DF
+#las funciones explode y split estan explicadas en
+#https://spark.apache.org/docs/2.2.0/api/python/pyspark.sql.html
+from pyspark.sql.functions import current_timestamp
+
+wordsDF = linesDF.select(
+    explode(
+        split(linesDF.value, ' ')
+    ).alias('palabra'), current_timestamp().alias('tiempo')
+)
+
+# Generamos el word count en tiempo de ejecución
+wordCountsDF = wordsDF.groupBy('palabra', 'tiempo').count()
+
+# Iniciamos la consuta que muestra por consola o almacena en memoria el word count. 
+# Trabajamos a partir del DataFrame que contiene la agrupación de las palabras y el numero de repeticiones
+# Utilizamos el formato memory para poder mostrarlo en Notebook, 
+#si ejecutamos en consola debemos poner el formato console
+query = wordCountsDF\
+    .writeStream\
+    .outputMode('complete')\
+    .option("checkpointLocation", "/home/sergio/Documents") \
+    .format("memory") \
+    .queryName("palabras") \
+    .start()
+
+#en una ejecución desde el terminal de sistema, necesitamos evitar que el programa finalice mientras 
+#se está ejecutando la consulta en un Thread separado y en segundo plano. 
+#query.awaitTermination() 
+
+from IPython.display import display, clear_output
+from time import sleep
+while True:
+    clear_output(wait=True)
+    display(query.status)
+    display(spark.sql('SELECT palabra, tiempo FROM palabras WHERE LENGTH(palabra) > 3').show())
+    sleep(5)
