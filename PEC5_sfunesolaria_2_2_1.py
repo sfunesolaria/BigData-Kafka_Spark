4d37e01f754116aef220bac01da33e86f4edc339
sfunesolaria <76247201+sfunesolaria@users.noreply.github.com>
2022-01-17 12:57:14 +0100

    Update PEC5_sfunesolaria_2_2_1.py

--- PEC5_sfunesolaria_2_2_1.py
+++ PEC5_sfunesolaria_2_2_1.py
@@ -1,54 +1,38 @@
 import findspark
 findspark.init()
 
-from pyspark import SparkConf, SparkContext, SQLContext, HiveContext
+from pyspark import SparkConf, SparkContext
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import explode
-from pyspark.sql.functions import split
 
 conf = SparkConf()
 conf.setMaster("local[1]")
 sc = SparkContext(conf=conf)
 print(sc.version)
 
-# Introducid el nombre de la app PEC5_ seguido de vuestro nombre de usuario
 spark = SparkSession \
     .builder \
     .appName("PEC5_sfunesolaria") \
     .getOrCreate()
 
-# Creamos el DataFrame representando el streaming de las lineas que nos entran por host:port
 linesDF = spark\
     .readStream\
     .format('rate')\
     .option('rowsPerSecond', '1')\
     .load()
 
-# Separamos las lineas en palabras en un nuevo DF
-#las funciones explode y split estan explicadas en
-#https://spark.apache.org/docs/2.2.0/api/python/pyspark.sql.html
 wordsDF = linesDF.withColumn("window", linesDF.timestamp)\
                 .withColumn("value", linesDF.value)
 
-# Generamos el word count en tiempo de ejecución
 from pyspark.sql.functions import window
 
 wordCountsDF = wordsDF.groupBy(window('window', "10 seconds", "5 seconds"), 'value').count()
 
-# Iniciamos la consuta que muestra por consola o almacena en memoria el word count. 
-# Trabajamos a partir del DataFrame que contiene la agrupación de las palabras y el numero de repeticiones
-# Utilizamos el formato memory para poder mostrarlo en Notebook, 
-#si ejecutamos en consola debemos poner el formato console
 query = wordCountsDF\
     .writeStream\
     .outputMode('complete')\
     .format("memory") \
     .queryName("palabras") \
     .start()
-
-#en una ejecución desde el terminal de sistema, necesitamos evitar que el programa finalice mientras 
-#se está ejecutando la consulta en un Thread separado y en segundo plano. 
-#query.awaitTermination() 
 
 from IPython.display import display, clear_output
 from time import sleep
